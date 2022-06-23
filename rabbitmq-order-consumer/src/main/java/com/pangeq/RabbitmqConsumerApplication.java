589e9968c56091739ea12edaa052b6023a65cb75
pangeq <pangeq17863416723@163.com>
2022-06-23 16:48:06 +0800

    rabbitmq

---
+++ rabbitmq-order-consumer/src/main/java/com/pangeq/RabbitmqConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.pangeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName RabbitmqConsumerApplication
+ * @Description
+ * @Author pangeq
+ * @Email erqing.pang@finchsoft.com.cn
+ * @Date 2022/6/23 15:56
+ */
+@SpringBootApplication
+public class RabbitmqConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqConsumerApplication.class,args);
+    }
+}
---
+++ rabbitmq-order-producer/src/main/java/com/pangeq/RabbitmqProducerApplication.java
@@ -0,0 +1,18 @@
+package com.pangeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName RabbitmqConsumerApplication
+ * @Description
+ * @Author pangeq
+ * @Email erqing.pang@finchsoft.com.cn
+ * @Date 2022/6/23 15:56
+ */
+@SpringBootApplication
+public class RabbitmqProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProducerApplication.class,args);
+    }
+}
